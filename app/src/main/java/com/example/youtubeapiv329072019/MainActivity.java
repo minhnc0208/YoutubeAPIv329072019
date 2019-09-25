17ad6ad731e0afe43f91a9cde840745f67e5b525
minhnc02082000 <53040823+minhnc02082000@users.noreply.github.com>
2019-09-25 18:45:36 +0700

    'A'

---
+++ app/src/main/java/com/example/youtubeapiv329072019/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.youtubeapiv329072019;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    String API_KEY = "AIzaSyBLwfipu-EMUnld2gvB3S4g3NrMwO5Ovks";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
